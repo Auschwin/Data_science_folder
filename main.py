74d16c8e9cc7d3c62362724a53629e29dd0f4cda
Auschwin <68668286+Auschwin@users.noreply.github.com>
2020-07-22 18:32:48 -0500

    my second commit

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ app = Flask(__name__, static_url_path='', static_folder='')
 def root():
     return app.send_static_file('event.html')
 
+Print('This is Auschwin')
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True
